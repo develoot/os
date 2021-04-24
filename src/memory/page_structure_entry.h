0452c0209178d4d7963d09f16711a2d00ec998ac
Kitae Kim <develoot@gmail.com>
2021-04-24 22:45:47 +0900

    Clean up paging

---
+++ src/memory/page_structure_entry.h
@@ -0,0 +1,104 @@
+#ifndef _MEMORY_PAGE_STRUCTURE_ENTRY_H
+#define _MEMORY_PAGE_STRUCTURE_ENTRY_H
+
+/**
+ * Bitmask definitions for each memory map page-structure entries.
+ *
+ * Bit fields not covered here are reserved or unused bits.
+ */
+
+/**
+ * Present Flag
+ *
+ * The processor seeks next paging structure if this bit is set.
+ * Otherwise, the processor generates page fault exception.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_PRESENT (0x0000000000000001)
+/**
+ * Read/Write Flag
+ *
+ * If unset, writes are not be allowed to the region controlled by this entry.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_READ_WRITE (0x0000000000000002)
+/**
+ * User/Supervisor Flag
+ *
+ * If unset, user-mode accesses are not allowed to the region controlled by this entry.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_USER_SUPER (0x0000000000000004)
+/**
+ * Page-Level Write-Through Flag
+ *
+ * Indirectly determines the memory type used to access the next paging structure referenced by this
+ * entry.
+ *
+ * Refer to section 4.5 of Intel 64 and IA-32 architectures software developer's manual for detailed
+ * description.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_CACHE_WRITE_THROUGH (0x0000000000000008)
+/**
+ * Page-Level Cache Disabled Flag
+ *
+ * Indirectly determines the memory type used to access the next paging structure referenced by this
+ * entry.
+ *
+ * Refer to section 4.5 of Intel 64 and IA-32 architectures software developer's manual for detailed
+ * description.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_CACHE_DISABLED (0x0000000000000010)
+/**
+ * Accessed Flag
+ *
+ * Indicates whether this entry has been used for linear-address translation.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_ACCESSED (0x0000000000000020)
+/**
+ * Dirty Flag
+ *
+ * Indicates whether software has written to the 4KB page referenced by this entry.
+ */
+#define PAGE_TABLE_ENTRY_DIRTY (0x0000000000000040)
+/**
+ * Page Size Flag
+ *
+ * Must be unset. otherwise, this entry dose not maps to 4KB page.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_PAGE_SIZE (0x0000000000000080)
+/**
+ * Page Access Type Flag
+ *
+ * Indirectly dertermines the memory type used to access the 4KB page referenced by this entry.
+ */
+#define PAGE_TABLE_ENTRY_ACCESS_TYPE (0x0000000000000080)
+/**
+ * Global Flag
+ *
+ * If CR4.PGE = 1, determines whether the translation is global. Ignored otherwise.
+ */
+#define PAGE_TABLE_ENTRY_GLOBAL (0x0000000000000100)
+/**
+ * Physical address of next paging structure referenced by this entry.
+ *
+ * Note that actual size of field that represents physical address varies based on the
+ * implementation of processor.
+ *
+ * The field 51:M where M is abbreviation for the highest bit of the address field are reserved
+ * so that they must be unset. If those bits are not unset, page fault exception occurs.
+ *
+ * The exact value of M can be found by using CPUID instruction. But, we just let M 48 for now.
+ */
+#define GENERAL_PAGE_STRUCTURE_ENTRY_BASE_ADDRESS          (0x000FFFFFFFFFF000)
+#define GENERAL_PAGE_STRUCTURE_ENTRY_BASE_ADDRESS_RESERVED (0x000F000000000000)
+/**
+ * Execution-Disable Flag
+ *
+ * If IA32_EFER.NXE = 1, disable execution on the region controlled by this structure.
+ *
+ * If set, instruction fetches are not allowed from the region controlled by this entry.
+ * Reserved otherwise.
+ */
+#define GENERAL_TABLE_ENTRY_EXECUTION_DISABLED (0x8000000000000000)
+
+#define GENERAL_PAGE_STRUCTURE_ENTRY_DEFAULT_FLAG (GENERAL_PAGE_STRUCTURE_ENTRY_READ_WRITE)
+
+#endif
--- src/memory/paging.c
+++ src/memory/paging.c
@@ -1,73 +1,77 @@
 #include <debug/assert.h>
 
 #include "page_frame_allocator.h"
-#include "page_table.h"
+#include "page_structure_entry.h"
+
 #include "paging.h"
 
-#define PAGE_NOT_PRESENT(table, offset) \
-    ((table[offset] & GENERAL_TABLE_ENTRY_PRESENT) == 0)
+#define PAGE_NOT_PRESENT(table, offset) (!(table[offset] & GENERAL_PAGE_STRUCTURE_ENTRY_PRESENT))
 
 extern struct page_frame_allocator_info global_page_frame_allocator_info;
 
-static inline uint16_t get_level4_offset(uint64_t virtual_address)
+static inline uint16_t get_level4_table_offset(uint64_t virtual_address)
 {
     return (virtual_address >> 39) & 0x1FF;
 }
 
-static inline uint16_t get_level3_offset(uint64_t virtual_address)
+static inline uint16_t get_level3_table_offset(uint64_t virtual_address)
 {
     return (virtual_address >> 30) & 0x1FF;
 }
 
-static inline uint16_t get_level2_offset(uint64_t virtual_address)
+static inline uint16_t get_level2_table_offset(uint64_t virtual_address)
 {
     return (virtual_address >> 21) & 0x1FF;
 }
 
-static inline uint16_t get_level1_offset(uint64_t virtual_address)
+static inline uint16_t get_level1_table_offset(uint64_t virtual_address)
 {
     return (virtual_address >> 12) & 0x1FF;
 }
 
-static inline void set_table(uint64_t *entry, uint64_t *table)
+static inline void set_next_page_structure(uint64_t *entry, uint64_t *next_page_structure)
 {
-    uint64_t table_address = (uint64_t)table;
+    uint64_t next_page_structure_address = (uint64_t)next_page_structure;
+    assert(next_page_structure_address % PAGE_SIZE == 0, "Page structure address not aligned");
 
-    assert(table_address % PAGE_SIZE == 0, "Request to set not aligned paging structure address");
-
-    *entry &= ~GENERAL_TABLE_ENTRY_BASE_ADDRESS;
-    *entry |= table_address;
-    *entry |= GENERAL_TABLE_ENTRY_PRESENT;
+    *entry &= ~GENERAL_PAGE_STRUCTURE_ENTRY_BASE_ADDRESS;
+    *entry |= next_page_structure_address;
+    *entry |= GENERAL_PAGE_STRUCTURE_ENTRY_PRESENT;
 }
 
-static inline uint64_t *get_table(uint64_t entry)
+static inline uint64_t *get_next_page_structure(uint64_t entry)
 {
-    return (uint64_t *)(entry & GENERAL_TABLE_ENTRY_BASE_ADDRESS);
+    return (uint64_t *)(entry & GENERAL_PAGE_STRUCTURE_ENTRY_BASE_ADDRESS);
 }
 
-static uint64_t *request_new_table()
+static uint64_t *request_new_page_structure(void)
 {
-    uint64_t *new_table = request_page_frame();
-    if (new_table == PAGE_FRAME_NULL) {
+    uint64_t *new_page_structure = request_page_frame();
+    if (new_page_structure == PAGE_FRAME_NULL) {
         return PAGE_NULL;
     }
 
+    uint64_t new_page_structure_address = (uint64_t)new_page_structure;
+    assert(new_page_structure_address % PAGE_FRAME_SIZE == 0, "New page structure not aligned");
+    assert(new_page_structure_address < GENERAL_PAGE_STRUCTURE_ENTRY_BASE_ADDRESS,
+            "New page structure address out of address space");
+
     for (uint64_t i = 0; i < 512; ++i) {
-        new_table[i] = 0;
-        new_table[i] |= PAGE_DEFAULT_ATTRIBUTE;
+        new_page_structure[i] = 0;
+        new_page_structure[i] |= GENERAL_PAGE_STRUCTURE_ENTRY_DEFAULT_FLAG;
     }
 
-    return new_table;
+    return new_page_structure;
 }
 
-static int add_new_table(uint64_t *entry)
+static int set_new_page_structure(uint64_t *entry)
 {
-    uint64_t *new_table = request_new_table();
-    if (new_table == PAGE_NULL) {
+    uint64_t *new_page_structure = request_new_page_structure();
+    if (new_page_structure == PAGE_NULL) {
         return 1;
     }
 
-    set_table(entry, new_table);
+    set_next_page_structure(entry, new_page_structure);
 
     return 0;
 }
@@ -77,12 +81,12 @@ int init_kernel_page_map(struct paging_info *paging_info)
     int result;
 
     if (paging_info->level4_table == PAGE_NULL) {
-        void *new_table = request_new_table();
-        if (new_table == PAGE_NULL) {
+        void *new_page_structure = request_new_page_structure();
+        if (new_page_structure == PAGE_NULL) {
             return 1;
         }
 
-        paging_info->level4_table = new_table;
+        paging_info->level4_table = new_page_structure;
     }
 
     for (uint64_t i = 0; i < global_page_frame_allocator_info.total_page_frame_number; ++i) {
@@ -98,41 +102,41 @@ int init_kernel_page_map(struct paging_info *paging_info)
 int map_page(struct paging_info *paging_info,
         uint64_t virtual_page_address, uint64_t physical_page_address)
 {
-    assert(virtual_page_address % PAGE_SIZE == 0, "Request to map not aligned virtual address");
-    assert(physical_page_address % PAGE_SIZE == 0, "Request to map not aligned physical address");
+    assert(virtual_page_address % PAGE_SIZE == 0, "Not aligned virtual address");
+    assert(physical_page_address % PAGE_SIZE == 0, "Not aligned physical address");
 
     int result;
 
     uint64_t *level4_table = paging_info->level4_table;
-    uint64_t level4_offset = get_level4_offset(virtual_page_address);
-    if (PAGE_NOT_PRESENT(level4_table, level4_offset)) {
-        result = add_new_table(&level4_table[level4_offset]);
+    uint64_t level4_table_offset = get_level4_table_offset(virtual_page_address);
+    if (PAGE_NOT_PRESENT(level4_table, level4_table_offset)) {
+        result = set_new_page_structure(&level4_table[level4_table_offset]);
         if (result != 0) {
             return 1;
         }
     }
 
-    uint64_t *level3_table = get_table(level4_table[level4_offset]);
-    uint64_t level3_offset = get_level3_offset(virtual_page_address);
-    if (PAGE_NOT_PRESENT(level3_table, level3_offset)) {
-        add_new_table(&level3_table[level3_offset]);
+    uint64_t *level3_table = get_next_page_structure(level4_table[level4_table_offset]);
+    uint64_t level3_table_offset = get_level3_table_offset(virtual_page_address);
+    if (PAGE_NOT_PRESENT(level3_table, level3_table_offset)) {
+        result = set_new_page_structure(&level3_table[level3_table_offset]);
         if (result != 0) {
             return 1;
         }
     }
 
-    uint64_t *level2_table = get_table(level3_table[level3_offset]);
-    uint64_t level2_offset = get_level2_offset(virtual_page_address);
-    if (PAGE_NOT_PRESENT(level2_table, level2_offset)) {
-        add_new_table(&level2_table[level2_offset]);
+    uint64_t *level2_table = get_next_page_structure(level3_table[level3_table_offset]);
+    uint64_t level2_table_offset = get_level2_table_offset(virtual_page_address);
+    if (PAGE_NOT_PRESENT(level2_table, level2_table_offset)) {
+        result = set_new_page_structure(&level2_table[level2_table_offset]);
         if (result != 0) {
             return 1;
         }
     }
 
-    uint64_t *level1_table = get_table(level2_table[level2_offset]);
-    uint64_t level1_offset = get_level1_offset(virtual_page_address);
-    set_table(&level1_table[level1_offset], (uint64_t *)physical_page_address);
+    uint64_t *level1_table = get_next_page_structure(level2_table[level2_table_offset]);
+    uint64_t level1_offset = get_level1_table_offset(virtual_page_address);
+    set_next_page_structure(&level1_table[level1_offset], (uint64_t *)physical_page_address);
 
     // TODO: Flush entries in the TLB.
 
@@ -142,10 +146,7 @@ int map_page(struct paging_info *paging_info,
 void change_current_page_map(struct paging_info *paging_info)
 {
     uint64_t level4_table_address = (uint64_t)paging_info->level4_table;
-
-    assert(level4_table_address % PAGE_SIZE == 0, "Request to use not aligned page map");
-    assert(level4_table_address < GENERAL_TABLE_ENTRY_BASE_ADDRESS,
-            "Reserved bits of table address are set to 1");
+    assert(level4_table_address % PAGE_SIZE == 0, "Not aligned PML4");
 
     asm __volatile__ (
         "mov %0, %%cr3\n\t"
