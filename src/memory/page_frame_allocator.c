fd762765cbf2d3655a2fbebcd93e6ba5dda40951
Kitae Kim <develoot@gmail.com>
2021-05-01 23:14:04 +0900

    Refactor the page frame allocator

--- src/memory/page_frame_allocator.c
+++ src/memory/page_frame_allocator.c
@@ -2,77 +2,66 @@
 
 #include <debug/assert.h>
 #include <general/boolean.h>
+#include <general/inline.h>
 
 #include "page_frame_allocator.h"
 
-struct page_frame_allocator_info global_page_frame_allocator_info;
+static struct page_frame_allocator_info global_page_frame_allocator_info;
 
-static inline uint64_t get_page_frame_index(page_frame_t page_frame)
+static always_inline uint64_t convert_address_to_index(uint64_t page_frame_address)
 {
-    uint64_t page_frame_address = (uint64_t)page_frame;
     assert(page_frame_address % PAGE_FRAME_SIZE == 0, "Not aligned page frame");
-
     return page_frame_address / PAGE_FRAME_SIZE;
 }
 
-static inline uint64_t get_page_frame_address(uint64_t page_frame_index)
+static always_inline uint64_t convert_index_to_address(uint64_t page_frame_index)
 {
-    assert(page_frame_index < PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER,
-            "Page frame index out of boundary");
-
+    assert(page_frame_index < PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER, "Page frame index too large");
     return page_frame_index * PAGE_FRAME_SIZE;
 }
 
-static uint8_t bit_masks[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
-
-static inline void set_bit(uint8_t *bitmap, uint64_t bit_index, enum boolean value)
+static always_inline void set_bit(uint8_t *const bitmap, uint64_t bit_index, enum boolean value)
 {
-    uint64_t bitmap_index = bit_index / 8;
-    bitmap[bitmap_index] &= ~bit_masks[bit_index % 8];
-    bitmap[bitmap_index] |= (value << (bit_index % 8));
+    bitmap[bit_index / 8] &= ~(1 << (bit_index % 8));
+    bitmap[bit_index / 8] |= (value << (bit_index % 8));
 }
 
-static inline void set_bits(uint8_t *bitmap, uint64_t bit_index, uint64_t size, enum boolean value)
+static always_inline void set_bits(uint8_t *const bitmap, uint64_t bit_index, uint64_t size,
+        enum boolean value)
 {
     for (uint64_t i = 0; i < size; ++i) {
         set_bit(bitmap, bit_index + i, value);
     }
 }
 
-static inline enum boolean get_bit(uint8_t *bitmap, uint64_t bit_index)
+static always_inline enum boolean get_bit(uint8_t *const bitmap, uint64_t bit_index)
 {
-    uint64_t bitmap_index = bit_index / 8;
-    return (bitmap[bitmap_index] & bit_masks[bit_index % 8]) > 0;
+    return (bitmap[bit_index / 8] & (1 << (bit_index % 8))) > 0;
 }
 
-static inline uint64_t get_total_page_frame_number(struct uefi_memory_map_info memory_map_info)
+static uint64_t get_total_uefi_frame_number(struct uefi_memory_map_info memory_map_info)
 {
-    uint64_t total_page_frame_number = 0;
+    uint64_t total_uefi_frame_number = 0;
 
     FOR_EACH_DESCRIPTOR(
-            d,
+            descriptor,
             memory_map_info.memory_descriptor_buffer,
             memory_map_info.memory_descriptor_buffer_size,
-            memory_map_info.memory_descriptor_size)
-    {
-        total_page_frame_number += d->NumberOfPages;
+            memory_map_info.memory_descriptor_size) {
+        total_uefi_frame_number += descriptor->NumberOfPages;
     }
 
-    return total_page_frame_number;
+    return total_uefi_frame_number;
 }
 
-static inline int init_page_frame_bitmap(uint8_t *bitmap,
-        struct uefi_memory_map_info memory_map_info)
+static int init_page_frame_bitmap(uint8_t *bitmap, struct uefi_memory_map_info memory_map_info)
 {
-    uint64_t bit_index = 0;
-
     FOR_EACH_DESCRIPTOR(
-            d,
+            descriptor,
             memory_map_info.memory_descriptor_buffer,
             memory_map_info.memory_descriptor_buffer_size,
-            memory_map_info.memory_descriptor_size)
-    {
-        switch (d->Type) {
+            memory_map_info.memory_descriptor_size) {
+        switch (descriptor->Type) {
         case EfiReservedMemoryType:
         case EfiLoaderCode:
         case EfiLoaderData:
@@ -83,16 +72,16 @@ static inline int init_page_frame_bitmap(uint8_t *bitmap,
         case EfiACPIMemoryNVS:
         case EfiMemoryMappedIO:
         case EfiMemoryMappedIOPortSpace:
-        case EfiPalCode:
+        case EfiPalCode: {
             break;
-
+        }
         case EfiBootServicesCode:
         case EfiBootServicesData:
-        case EfiConventionalMemory:
-            bit_index = get_page_frame_index((page_frame_t)d->PhysicalStart);
-            set_bits(bitmap, bit_index, d->NumberOfPages, false);
+        case EfiConventionalMemory: {
+            uint64_t bit_index = convert_address_to_index(descriptor->PhysicalStart);
+            set_bits(bitmap, bit_index, descriptor->NumberOfPages, false);
             break;
-
+        }
         default:
             return 1;
         }
@@ -103,21 +92,22 @@ static inline int init_page_frame_bitmap(uint8_t *bitmap,
 
 int init_page_frame_allocator(struct uefi_memory_map_info memory_map_info)
 {
-    uint64_t total_page_frame_number = get_total_page_frame_number(memory_map_info);
-    if (total_page_frame_number > PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER) {
-        total_page_frame_number = PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER;
+    uint64_t total_uefi_frame_number = get_total_uefi_frame_number(memory_map_info);
+    if (total_uefi_frame_number <= 0) {
+        return 1;
+    }
+    if (total_uefi_frame_number > PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER) {
+        total_uefi_frame_number = PAGE_FRAME_BITMAP_MAX_PAGE_NUMBER;
     }
 
-    global_page_frame_allocator_info.bitmap_size = (total_page_frame_number % 8) > 0
-            ? (total_page_frame_number / 8) + 1
-            : (total_page_frame_number / 8);
+    global_page_frame_allocator_info.bitmap_size = ((total_uefi_frame_number - 1) / 8) + 1;
 
     for (uint64_t i = 0; i < global_page_frame_allocator_info.bitmap_size; ++i) {
         global_page_frame_allocator_info.bitmap[i] = 0xFF;
     }
 
-    global_page_frame_allocator_info.total_page_frame_number = total_page_frame_number;
-    global_page_frame_allocator_info.free_page_frame_number = total_page_frame_number;
+    global_page_frame_allocator_info.total_page_frame_number = total_uefi_frame_number;
+    global_page_frame_allocator_info.free_page_frame_number = total_uefi_frame_number;
 
     int result = init_page_frame_bitmap(global_page_frame_allocator_info.bitmap, memory_map_info);
     if (result != 0) {
@@ -127,10 +117,15 @@ int init_page_frame_allocator(struct uefi_memory_map_info memory_map_info)
     return 0;
 }
 
+uint64_t get_total_page_frame_number(void)
+{
+    return global_page_frame_allocator_info.total_page_frame_number;
+}
+
 page_frame_t request_page_frame(void)
 {
     page_frame_t new_page_frame = PAGE_FRAME_NULL;
-    uint8_t *bitmap = global_page_frame_allocator_info.bitmap;
+    uint8_t *const bitmap = global_page_frame_allocator_info.bitmap;
 
     if (global_page_frame_allocator_info.free_page_frame_number == 0) {
         return PAGE_FRAME_NULL;
@@ -138,15 +133,13 @@ page_frame_t request_page_frame(void)
 
     for (uint64_t bit_index = 0;
             bit_index < global_page_frame_allocator_info.total_page_frame_number;
-            ++bit_index)
-    {
+            ++bit_index) {
         if (get_bit(bitmap, bit_index) == true) {
             continue;
         }
-
         set_bit(bitmap, bit_index, true);
         --global_page_frame_allocator_info.free_page_frame_number;
-        new_page_frame = (page_frame_t)get_page_frame_address(bit_index);
+        new_page_frame = (page_frame_t)convert_index_to_address(bit_index);
         break;
     }
 
@@ -158,8 +151,10 @@ void free_page_frame(page_frame_t page_frame)
     uint64_t page_frame_address = (uint64_t)page_frame;
     assert(page_frame_address % PAGE_FRAME_SIZE == 0, "Not aligned page frame");
 
-    uint8_t *bitmap = global_page_frame_allocator_info.bitmap;
-    uint64_t page_frame_index = get_page_frame_index(page_frame);
+    uint8_t *const bitmap = global_page_frame_allocator_info.bitmap;
+    uint64_t page_frame_index = convert_address_to_index(page_frame_address);
+    assert(get_bit(bitmap, page_frame_index) == true, "Double-free page frame");
+
     set_bit(bitmap, page_frame_index, false);
     ++global_page_frame_allocator_info.free_page_frame_number;
 }
--- src/memory/page_frame_allocator.h
+++ src/memory/page_frame_allocator.h
@@ -5,6 +5,7 @@
 #include "page_frame_allocator_type.h"
 
 int init_page_frame_allocator(struct uefi_memory_map_info memory_map_info);
+uint64_t get_total_page_frame_number(void);
 page_frame_t request_page_frame(void);
 void free_page_frame(page_frame_t page_frame);
 
