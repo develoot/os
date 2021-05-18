4326dfe7ab7c7f63d141db6a0740f1392f124f76
Kitae Kim <develoot@gmail.com>
2021-05-18 15:59:43 +0900

    Initialize IDT using segment selector macro

--- lib/interrupts/init.c
+++ lib/interrupts/init.c
@@ -1,3 +1,6 @@
+#include <memory/segmentation/global_descriptor_table.h>
+#include <memory/segmentation/segment_selector.h>
+
 #include "interrupt_descriptor_table.h"
 #include "exception_handlers.h"
 #include "init.h"
@@ -16,26 +19,65 @@ static struct interrupt_descriptor_table global_interrupt_descriptor_table;
 
 int init_interrupts(void)
 {
-    register_exception_handler(divide_error, dummy_handler, 0, 0);
-    register_exception_handler(debug, dummy_handler, 0, 0);
-    register_exception_handler(non_maskable_interrupt, dummy_handler, 0, 0);
-    register_exception_handler(break_point, dummy_handler, 0, 0);
-    register_exception_handler(overflow, dummy_handler, 0, 0);
-    register_exception_handler(bound_range_exceeded, dummy_handler, 0, 0);
-    register_exception_handler(invalid_opcode, dummy_handler, 0, 0);
-    register_exception_handler(device_not_available, dummy_handler, 0, 0);
-    register_exception_handler(double_fault, dummy_handler, 0, 0);
-    register_exception_handler(coprocessor_segment_overrun, dummy_handler, 0, 0);
-    register_exception_handler(invalid_task_state_segment, dummy_handler, 0, 0);
-    register_exception_handler(segment_not_present, dummy_handler, 0, 0);
-    register_exception_handler(stack_full, dummy_handler, 0, 0);
-    register_exception_handler(general_protection, dummy_handler, 0, 0);
-    register_exception_handler(page_fault, dummy_handler, 0, 0);
-    register_exception_handler(x87_fpu_floating_point_error, dummy_handler, 0, 0);
-    register_exception_handler(alignment_check, dummy_handler, 0, 0);
-    register_exception_handler(machine_check, dummy_handler, 0, 0);
-    register_exception_handler(simd_floating_point, dummy_handler, 0, 0);
-    register_exception_handler(virtualization, dummy_handler, 0, 0);
+    register_exception_handler(divide_error, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(debug, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(non_maskable_interrupt, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(break_point, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(overflow, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(bound_range_exceeded, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(invalid_opcode, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(device_not_available, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(double_fault, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(coprocessor_segment_overrun, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(invalid_task_state_segment, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(segment_not_present, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(stack_full, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(general_protection, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(page_fault, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(x87_fpu_floating_point_error, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(alignment_check, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(machine_check, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(simd_floating_point, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
+
+    register_exception_handler(virtualization, dummy_handler,
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0), 0);
 
     return 0;
 }
