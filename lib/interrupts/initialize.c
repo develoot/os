3397c8546ffb0eb91d0fdc788a3470c7387b8825
Kitae Kim <develoot@gmail.com>
2021-05-19 15:08:40 +0900

    Clean up "interrupts/initialize.c"

--- lib/interrupts/initialize.c
+++ lib/interrupts/initialize.c
@@ -6,98 +6,44 @@
 #include "interrupt_descriptor_table.h"
 #include "initialize.h"
 
-#define register_exception_handler(Exception, Handler, Selector, Attribute)                         \
-({                                                                                                  \
-    global_interrupt_descriptor_table.Exception.offset0 = (address_t)(Handler) & 0xFFFF;            \
-    global_interrupt_descriptor_table.Exception.offset1 = ((address_t)(Handler) >> 16) & 0xFFFF;    \
-    global_interrupt_descriptor_table.Exception.offset2 = ((address_t)(Handler) >> 32) & 0xFFFFFFF; \
-    global_interrupt_descriptor_table.Exception.segment_selector = (Selector);                      \
-    global_interrupt_descriptor_table.Exception.attribute = (Attribute);                            \
+#define register_interrupt_handler(Interrupt, Handler, Selector, Attribute) ({                      \
+    global_interrupt_descriptor_table.Interrupt.offset0 = (address_t)(Handler) & 0xFFFF;            \
+    global_interrupt_descriptor_table.Interrupt.offset1 = ((address_t)(Handler) >> 16) & 0xFFFF;    \
+    global_interrupt_descriptor_table.Interrupt.offset2 = ((address_t)(Handler) >> 32) & 0xFFFFFFF; \
+    global_interrupt_descriptor_table.Interrupt.segment_selector = (Selector);                      \
+    global_interrupt_descriptor_table.Interrupt.attribute = (Attribute);                            \
+})
+
+#define register_exception_handler(Exception, Handler) ({                                           \
+    register_interrupt_handler(Exception, Handler,                                                  \
+            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),              \
+            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));   \
 })
 
 static struct interrupt_descriptor_table global_interrupt_descriptor_table;
 
 int initialize_interrupts(void)
 {
-    register_exception_handler(divide_error, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(debug, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(non_maskable_interrupt, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(break_point, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(overflow, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(bound_range_exceeded, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(invalid_opcode, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(device_not_available, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(double_fault, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(coprocessor_segment_overrun, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(invalid_task_state_segment, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(segment_not_present, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(stack_full, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(general_protection, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(page_fault, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(x87_fpu_floating_point_error, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(alignment_check, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(machine_check, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(simd_floating_point, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
-
-    register_exception_handler(virtualization, dummy_handler,
-            segment_selector(GLOBAL_DESCRIPTOR_TABLE_KERNEL_CODE_SEGMENT_INDEX, 0, 0),
-            interrupt_gate_descriptor_attribute(1, INTERRUPT_GATE_DESCRIPTOR_TYPE_INTERRUPT, 0));
+    register_exception_handler(divide_error,                 dummy_handler);
+    register_exception_handler(debug,                        dummy_handler);
+    register_exception_handler(non_maskable_interrupt,       dummy_handler);
+    register_exception_handler(break_point,                  dummy_handler);
+    register_exception_handler(overflow,                     dummy_handler);
+    register_exception_handler(bound_range_exceeded,         dummy_handler);
+    register_exception_handler(invalid_opcode,               dummy_handler);
+    register_exception_handler(device_not_available,         dummy_handler);
+    register_exception_handler(double_fault,                 dummy_handler);
+    register_exception_handler(coprocessor_segment_overrun,  dummy_handler);
+    register_exception_handler(invalid_task_state_segment,   dummy_handler);
+    register_exception_handler(segment_not_present,          dummy_handler);
+    register_exception_handler(stack_full,                   dummy_handler);
+    register_exception_handler(general_protection,           dummy_handler);
+    register_exception_handler(page_fault,                   dummy_handler);
+    register_exception_handler(x87_fpu_floating_point_error, dummy_handler);
+    register_exception_handler(alignment_check,              dummy_handler);
+    register_exception_handler(machine_check,                dummy_handler);
+    register_exception_handler(simd_floating_point,          dummy_handler);
+    register_exception_handler(virtualization,               dummy_handler);
 
     struct interrupt_descriptor_table_register_entry register_entry = {
         .table_limit = sizeof(global_interrupt_descriptor_table) - 1,
