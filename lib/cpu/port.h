4aae1291a128c0e5a26a41e74cd48faa432010cd
Kitae Kim <develoot@gmail.com>
2021-05-20 14:45:42 +0900

    Define ports for PIC

--- lib/cpu/port.h
+++ lib/cpu/port.h
@@ -5,7 +5,14 @@
 
 enum port {
     keyboard_port0 = 0x60,
-    keyboard_port1 = 0x64
+    keyboard_port1 = 0x64,
+
+    // PIC stands for "Programmable Interrupt Controller".
+    // For the 8259A interrupt controller.
+    pic_master_port0 = 0x20,
+    pic_master_port1 = 0x21,
+    pic_slave_port0  = 0xA0,
+    pic_slave_port1  = 0xA1
 };
 
 uint8_t read_port(enum port port);
