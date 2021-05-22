c958f33c9e858e4243eb0cb86952643ad12e86be
Kitae Kim <develoot@gmail.com>
2021-05-22 22:48:12 +0900

    Rewrite `_start()` using `assert()`

--- src/kernel/start.c
+++ src/kernel/start.c
@@ -1,3 +1,5 @@
+#include <debug/assert.h>
+
 #include <kernel/boot_data.h>
 #include <kernel/error.h>
 #include <kernel/shell.h>
@@ -13,18 +15,14 @@ int _start(const struct kernel_boot_data boot_data)
     initialize_print(boot_data.frame_buffer_data, boot_data.psf1_data);
 
     int result = initialize_page_frame_allocator(boot_data.memory_map_data);
-    if (KERNEL_ERROR(result)) {
-        print_string("InitPFAllocError ");
-        while (1) {}
-    }
+    assert(!KERNEL_ERROR(result), "Failed to initialize the page frame allocator.");
 
     struct paging_data kernel_paging_data = { .level4_table = PAGE_NULL };
     result = initialize_kernel_page_map(&kernel_paging_data);
-    if (KERNEL_ERROR(result)) {
-        print_string("InitPageMapError ");
-        while (1) {}
-    }
+    assert(!KERNEL_ERROR(result), "Failed to initialize the paging.");
+
     change_current_page_map(kernel_paging_data);
+
     initialize_segmentation();
     initialize_interrupts();
 
