e94270d6e3e5114950a7da02989ad2fe432c47b2
Kitae Kim <develoot@gmail.com>
2021-05-29 15:29:50 +0900

    Add methods to read and modify the linked list

--- modules/general/linked_list.h
+++ modules/general/linked_list.h
@@ -11,7 +11,7 @@
 #define container_of(MemberAddress, ContainerType, MemberName) \
     ((ContainerType *)((address_t)(MemberAddress) - offsetof(ContainerType, MemberName)))
 
-#define linked_list_for_each(Cursor, Head) \
+#define linked_list_for_each_node(Cursor, Head) \
     for (Cursor = (Head)->next; Cursor != (Head); Cursor = Cursor->next)
 
 struct linked_list_node {
@@ -34,6 +34,31 @@ always_inline void linked_list_append(struct linked_list_node *const head,
     node->previous = last;
 }
 
+always_inline struct linked_list_node *linked_list_get(struct linked_list_node *const head,
+        const uint64_t index)
+{
+    struct linked_list_node *cursor = head;
+
+    for (uint64_t i = 0; i < index; ++i) {
+        cursor = cursor->next;
+    }
+
+    return cursor;
+}
+
+always_inline void linked_list_insert(struct linked_list_node *const head, const uint64_t index,
+        struct linked_list_node *const new_node)
+{
+    struct linked_list_node *const current = linked_list_get(head, index);
+    struct linked_list_node *const previous = current->previous;
+
+    new_node->previous = previous;
+    previous->next = new_node;
+
+    new_node->next = current;
+    current->previous = new_node;
+}
+
 always_inline void linked_list_remove(struct linked_list_node *const node)
 {
     struct linked_list_node *const previous = node->previous;
@@ -53,7 +78,7 @@ always_inline size_t linked_list_size(struct linked_list_node *const head)
     size_t size = 0;
     struct linked_list_node *cursor = NULL;
 
-    linked_list_for_each(cursor, head) {
+    linked_list_for_each_node(cursor, head) {
         size++;
     }
 
