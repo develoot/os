4c1b526319bc792d23069f279309916a9939f42c
Kitae Kim <develoot@gmail.com>
2021-05-29 12:42:18 +0900

    Inline linked list methods
    I expect that these methods will be invoked frequently in not diverse places.

--- modules/general/linked_list.h
+++ modules/general/linked_list.h
@@ -6,6 +6,7 @@
 #include <stdint.h>
 
 #include <general/address.h>
+#include <general/inline.h>
 
 #define container_of(MemberAddress, ContainerType, MemberName) \
     ((ContainerType *)((address_t)(MemberAddress) - offsetof(ContainerType, MemberName)))
@@ -18,14 +19,45 @@ struct linked_list_node {
     struct linked_list_node *previous;
 };
 
-void linked_list_initialize(struct linked_list_node *const head);
+always_inline void linked_list_initialize(struct linked_list_node *const head)
+{
+    head->next = head;
+    head->previous = head;
+}
 
-void linked_list_append(struct linked_list_node *const head, struct linked_list_node *const node);
+always_inline void linked_list_append(struct linked_list_node *const head,
+        struct linked_list_node *const node)
+{
+    struct linked_list_node *const last = head->previous;
 
-void linked_list_remove(struct linked_list_node *const node);
+    last->next = node;
+    node->previous = last;
+}
 
-bool linked_list_is_empty(struct linked_list_node *const head);
+always_inline void linked_list_remove(struct linked_list_node *const node)
+{
+    struct linked_list_node *const previous = node->previous;
+    struct linked_list_node *const next = node->next;
 
-size_t linked_list_size(struct linked_list_node *const head);
+    previous->next = next;
+    next->previous = previous;
+}
+
+always_inline bool linked_list_is_empty(struct linked_list_node *const head)
+{
+    return head->next == head;
+}
+
+always_inline size_t linked_list_size(struct linked_list_node *const head)
+{
+    size_t size = 0;
+    struct linked_list_node *cursor = NULL;
+
+    linked_list_for_each(cursor, head) {
+        size++;
+    }
+
+    return size;
+}
 
 #endif
