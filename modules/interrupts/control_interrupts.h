8bd3b2f6705028b65b4ab00f5aae22726aa0fbb7
Kitae Kim <develoot@gmail.com>
2021-05-30 15:52:28 +0900

    Make functions to control interrupts inline

--- modules/interrupts/control_interrupts.h
+++ modules/interrupts/control_interrupts.h
@@ -3,8 +3,29 @@
 
 #include <stdint.h>
 
-void enable_interrupts(void);
-void disable_interrupts(void);
-uint64_t read_rflags(void);
+#include <general/inline.h>
+
+always_inline void enable_interrupts(void)
+{
+    asm __volatile__("sti");
+}
+
+always_inline void disable_interrupts(void)
+{
+    asm __volatile__("cli");
+}
+
+always_inline uint64_t read_rflags(void)
+{
+    uint64_t result = 0;
+
+    asm __volatile__(
+        "pushfq \n\t"
+        "pop %0 \n\t"
+        : "=m"(result)
+    );
+
+    return result;
+}
 
 #endif
