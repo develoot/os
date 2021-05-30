bf14d6a499e758ce748494570ca3815e8ca4ab3c
Kitae Kim <develoot@gmail.com>
2021-05-30 16:00:02 +0900

    Disable interrupts when entering handler

--- modules/interrupts/handlers/dummy_exception_handler.c
+++ modules/interrupts/handlers/dummy_exception_handler.c
@@ -1,9 +1,14 @@
 #include <kernel/print.h>
+#include <interrupts/control_interrupts.h>
 
 #include "dummy_exception_handler.h"
 
 void dummy_exception_handler(const uint8_t exception_number, const uint64_t error_code)
 {
+    disable_interrupts();
+
     print_format("Exception %u Error code %lu ", (uint32_t)exception_number, error_code);
     while (1) {}
+
+    enable_interrupts();
 }
--- modules/interrupts/handlers/dummy_interrupt_handler.c
+++ modules/interrupts/handlers/dummy_interrupt_handler.c
@@ -1,13 +1,18 @@
 #include <kernel/print.h>
 
+#include <interrupts/control_interrupts.h>
 #include <interrupts/programmable_interrupt_controller.h>
 
 #include "dummy_interrupt_handler.h"
 
 void dummy_interrupt_handler(const uint8_t interrupt_number)
 {
+    disable_interrupts();
+
     print_format("Interrupt %u ", (uint32_t)interrupt_number);
 
     // The exception vector size is 32 in the IA-32e architecture.
     notify_end_of_interrupt(interrupt_number - 32);
+
+    enable_interrupts();
 }
