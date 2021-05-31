6ceb6565424462c7aba1047b118582f9589a752e
Kitae Kim <develoot@gmail.com>
2021-05-31 22:40:14 +0900

    Pause the system in exception handler

--- modules/interrupts/dummy_exception_handler.c
+++ modules/interrupts/dummy_exception_handler.c
@@ -7,6 +7,7 @@
 always_inline void _dummy_exception_handler(const uint8_t exception_number, const uint64_t error_code)
 {
     print_format("Exception %u Error code %lu ", (uint32_t)exception_number, error_code);
+    while (1);
 }
 
 void dummy_exception_handler(const uint8_t exception_number, const uint64_t error_code)
