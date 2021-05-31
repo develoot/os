714942a8556cf601f3d408c35e435fa5ad60ed8c
Kitae Kim <develoot@gmail.com>
2021-05-31 22:29:00 +0900

    Remove unused file "port.c"

---
+++ modules/cpu/port.d
@@ -0,0 +1,4 @@
+/home/develoot/workspace/os/modules/cpu/port.d /home/develoot/workspace/os/modules/cpu/port.o: \
+ /home/develoot/workspace/os/modules/cpu/port.c \
+ /home/develoot/workspace/os/modules/cpu/port.h \
+ /home/develoot/workspace/os/modules/general/inline.h
